560ed9e1a066d05f5b6ec13103c00e594d47adae
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-06-08 00:38:25 -0700

    Final Commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,13 @@ import StockManagerSelector from "./sub_component/StockManagerSelector";
 
 const NavBar = () => {
     return (
-        <div>
-            <nav>
-                <h2>Inventory Manager</h2>
-
-                <Link to="/" style={{ 'fontWeight': 'bold', 'textDecoration': 'none' }}>Dashboard</Link>
-                <Link to="contact" style={{ 'fontWeight': 'bold', 'textDecoration': 'none' }}>Contact</Link>
-                
-                <StockManagerSelector />
-            </nav>
-        </div>
+        <nav>
+            <h2>Inventory Manager</h2>
+            <Link to="/" style={{ 'fontWeight': 'bold', 'textDecoration': 'none' }}>Dashboard</Link>
+            <Link to="contact" style={{ 'fontWeight': 'bold', 'textDecoration': 'none' }}>Contact</Link>
+            
+            <StockManagerSelector />
+        </nav>
     );
 }
 
